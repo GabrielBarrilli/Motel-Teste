63d59089f77363b3d8101ee45fb19faef8dbdf29
Barrilli01 <gabrielbarrillicruz@gmail.com>
2024-01-10 23:01:43 -0300

    Teste Commit

--- src/main/java/org/gabrielbarrilli/motelteste/repository/EntradaRepository.java
+++ src/main/java/org/gabrielbarrilli/motelteste/repository/EntradaRepository.java
@@ -12,7 +12,6 @@ import java.util.List;
 public interface EntradaRepository extends JpaRepository<Entrada, Long> {
     List<Entrada> findByDataRegistroEntrada(LocalDate dataRegistroEntrada);
     List<Entrada> findByStatusEntrada(LocalDate statusEntrada);
-    List<Entrada> findByTipoPagamento(TipoPagamento tipoPagamento);
     List<Entrada> findByPlaca(String placa);
     List<Entrada> findByHoraSaida(LocalTime horaSaida);
     List<Entrada> findByStatusPagamento(StatusPagamento statusPagamento);
