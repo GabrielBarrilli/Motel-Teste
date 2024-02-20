a5bec46c6eaabab6d097bebd11cac1c074bfd66a
Barrilli01 <gabrielbarrillicruz@gmail.com>
2024-02-20 09:03:05 -0300

    Implementação de testes de QuartosService finalizada 100%

--- src/test/java/org/gabrielbarrilli/motelteste/service/QuartosServiceTest.java
+++ src/test/java/org/gabrielbarrilli/motelteste/service/QuartosServiceTest.java
@@ -1,7 +1,12 @@
 package org.gabrielbarrilli.motelteste.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.assertj.core.api.Assertions;
+import org.gabrielbarrilli.motelteste.fixture.CreateQuartoRequest;
 import org.gabrielbarrilli.motelteste.fixture.QuartosFixture;
 import org.gabrielbarrilli.motelteste.model.Quartos;
+import org.gabrielbarrilli.motelteste.model.builders.QuartosBuilder;
+import org.gabrielbarrilli.motelteste.model.request.RegistrarQuartoRequest;
 import org.gabrielbarrilli.motelteste.repository.QuartosRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +17,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static java.util.Optional.ofNullable;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
@@ -27,13 +33,14 @@ class QuartosServiceTest {
     Quartos quartoDisponivel = QuartosFixture.quartoDisponivel();
     Quartos quartoOcupado = QuartosFixture.quartoOcupado();
     List<Quartos> quartosList = QuartosFixture.quartosList();
+    RegistrarQuartoRequest quartoRequest = CreateQuartoRequest.registrarQuartoRequest();
 
     @BeforeEach
     void setUp() {
     }
 
     @Test
-    void findAll() {
+    void testFindAll() {
         when(quartosRepository.findAll()).thenReturn(quartosList);
 
         quartosService.findAll();
@@ -42,6 +49,34 @@ class QuartosServiceTest {
     }
 
     @Test
-    void createQuarto() {
+    void testFindById() {
+        when(quartosRepository.findById(anyLong())).thenReturn(ofNullable(quartoDisponivel));
+
+        quartosService.findById(quartoDisponivel.getId());
+
+        verify(quartosRepository, atLeastOnce()).findById(quartoDisponivel.getId());
+    }
+
+    @Test
+    void testNotFindId() {
+
+        Assertions.assertThatExceptionOfType(EntityNotFoundException.class)
+                .isThrownBy(() -> quartosService.findById(anyLong()))
+                .withMessage("Quarto não encontrado!");
+    }
+
+    @Test
+    void testCreateQuarto() {
+
+        when(quartosRepository.save(any(Quartos.class))).thenReturn(quartoDisponivel);
+
+        var quarto = quartosService.createQuarto(quartoRequest);
+
+        assertThat(quarto.getId()).isNotNull();
+        assertThat(quarto.getNumero()).isEqualTo(quarto.getId());
+        assertThat(quarto.getDescricao()).isEqualTo(quartoRequest.descricao());
+        assertThat(quarto.getCapacidadePessoa()).isEqualTo(quartoRequest.capacidadePessoa());
+
+        verify(quartosRepository, atLeastOnce()).save(any(Quartos.class));
     }
 }
